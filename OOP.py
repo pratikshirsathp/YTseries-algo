d199267cef638e90a85c51def35a500df1e9d8d0
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-21 16:14:34 +0530

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,13 +1,15 @@
-#class
+#class blueprint
 class Computer:
 
   #will always get called
-  #sef refer to the object
+  #self refer to the object This is similar to this pointer in C++ and this reference in Java.
+
+  #init is kinda constructor
   def __init__(self, cpu, ram):
     self.cpu = cpu
     self.ram = ram
     
-  #objects
+  #objects instance
   def config(self):
     print("config is :", self.cpu, self.ram)
 
@@ -16,3 +18,14 @@ comp2 = Computer('i7',8)
 
 comp1.config() # we are passing comp1 in config
 comp2.config() # we are passing comp2 in config
+
+''''
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print('hey!',self.name) 
+
+p = Person('Pratik')
+p.greet()'''
