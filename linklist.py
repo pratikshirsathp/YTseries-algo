e1084c86e313a5852643de3016b169da0ce229e3
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-22 12:11:52 +0530

    Update linklist.py

--- linklist.py
+++ linklist.py
@@ -1,6 +1,26 @@
 class Node:
+  #create node
   def __inti__(self,item):
     self.item = item
     self.next = None
-    
+
+class LinkedList:
+  def __init__(self)
+    self.head = None
+
+if __name__ == '__main__':
   
+  linkedlist = LinkedList()
+  
+ #assign item values
+  linkedlist.head() = Node(1)
+  second = Node(2)
+  third = Node(3)
+  
+ # connect nodes
+  linkedlist.head.next() = second
+  second.next  = third
+  
+  while linked_list.head != None:
+        print(linked_list.head.item, end=" ")
+        linked_list.head = linked_list.head.next
