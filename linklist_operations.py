e35f24ab05cdda84d23ebfaf7bb98105200c6a3d
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-22 13:54:48 +0530

    Create linklist_operations.py

---
+++ linklist_operations.py
@@ -0,0 +1,12 @@
+class Node:
+  def __init__(self,item):
+    self.item = item
+    self.next = None
+   
+class LinkedList:
+  
+  def __init__(self):
+    self.head = None
+    
+    #insert at beginning
+  def InsertAtBeginning(self, data)
